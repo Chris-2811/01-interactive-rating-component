6d45e1158d6df940cb446d6eb98e02fb87fcc665
Chris <christophschmid01@gmail.com>
2023-07-10 15:31:11 +0200

    fixed img path

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
-const ratings = document.querySelectorAll('.rating')
-const submit = document.getElementById('submit')
-const modal = document.getElementById('modal')
-const container = document.getElementById('container')
-const ratingText = document.getElementById('rating-text')
+const ratings = document.querySelectorAll('.rating');
+const submit = document.getElementById('submit');
+const modal = document.getElementById('modal');
+const container = document.getElementById('container');
+const ratingText = document.getElementById('rating-text');
 
 let selected = false;
 
 // Add EventListener
-ratings.forEach(rating=> {
-    rating.addEventListener('click', e=> {
-        ratings.forEach(rating => {
-            rating.classList.remove('selected')
-        })
-        e.target.classList.add('selected')
-    })
-})
+ratings.forEach((rating) => {
+  rating.addEventListener('click', (e) => {
+    ratings.forEach((rating) => {
+      rating.classList.remove('selected');
+    });
+    e.target.classList.add('selected');
+  });
+});
 
-submit.addEventListener('click', e=> {
-    const selected = document.querySelectorAll('.rating.selected');
+submit.addEventListener('click', (e) => {
+  const selected = document.querySelectorAll('.rating.selected');
 
-    console.log(selected[0].value)
+  console.log(selected[0].value);
 
+  if (selected.length === 1) {
+    container.style.display = 'none';
+    modal.style.display = 'block';
 
-    
-    if(selected.length === 1) {
-        container.style.display = 'none'
-        modal.style.display = 'block'
-
-        ratingText.innerText = `You selected ${selected[0].value} out of 5`
-    }
-})
+    ratingText.innerText = `You selected ${selected[0].value} out of 5`;
+  }
+});
