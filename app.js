45ac87b7aee404a651e2a6243773156113276959
Chris <christophschmid01@gmail.com>
2023-07-10 15:18:14 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const ratings = document.querySelectorAll('.rating')
+const submit = document.getElementById('submit')
+const modal = document.getElementById('modal')
+const container = document.getElementById('container')
+const ratingText = document.getElementById('rating-text')
+
+let selected = false;
+
+// Add EventListener
+ratings.forEach(rating=> {
+    rating.addEventListener('click', e=> {
+        ratings.forEach(rating => {
+            rating.classList.remove('selected')
+        })
+        e.target.classList.add('selected')
+    })
+})
+
+submit.addEventListener('click', e=> {
+    const selected = document.querySelectorAll('.rating.selected');
+
+    console.log(selected[0].value)
+
+
+    
+    if(selected.length === 1) {
+        container.style.display = 'none'
+        modal.style.display = 'block'
+
+        ratingText.innerText = `You selected ${selected[0].value} out of 5`
+    }
+})
